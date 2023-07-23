22aae067ed230a22064abb0e1c17bea7d7ee0e8b
Rafael Castro <r.castro@pagadito.com>
2023-07-22 21:25:18 -0600

    Initial Commit

---
+++ Circulares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Circulares
+//
+//  Created by Rafael David Castro Luna on 22/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CircularesUITests/CircularesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CircularesUITestsLaunchTests.swift
+//  CircularesUITests
+//
+//  Created by Rafael David Castro Luna on 22/7/23.
+//
+
+import XCTest
+
+final class CircularesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
